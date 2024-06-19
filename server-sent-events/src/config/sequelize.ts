0c5fea276bcaa451197b0eee2b7f9039d65b6d5b
max esteban poblete pizarro <max.poblete2905@gmail.com>
2024-06-19 00:18:11 -0400

    adjustment docker compose

--- server-sent-events/src/config/sequelize.ts
+++ server-sent-events/src/config/sequelize.ts
@@ -1,13 +1,21 @@
 import { Sequelize } from 'sequelize-typescript';
 import { Transfer } from '../models/Transfer';
 
+const {
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
+} = process.env;
+
 export const sequelize = new Sequelize({
   dialect: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  database: 'transfers',
-  username: 'user',
-  password: 'password',
+  host: DATABASE_HOST,
+  port: parseInt(DATABASE_PORT),
+  database: DATABASE_NAME,
+  username: DATABASE_USER,
+  password: DATABASE_PASSWORD,
   models: [Transfer],
   define: {
     timestamps: false,
